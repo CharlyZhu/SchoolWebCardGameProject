72c656dc6fb6bcb78cb8226acfa315c2096227ad
unknown <rowanspenc@gmail.com>
2020-03-04 00:42:54 +0000

    Loader change

--- client-side/src/scenes/Loader.ts
+++ client-side/src/scenes/Loader.ts
@@ -1,27 +1,18 @@
 import { ICard } from "../card/CardManager";
 
 export class Loader extends Phaser.Scene {
+  constructor() {
+    super("loader");
+  }
 
-    constructor() {
-        super("loader")
+  public preload() {
+    const loaderPrefix: string = "assets/cards/json/";
+    this.load.json("card0001", loaderPrefix + "000" + 2 + ".json");
+    // this.load.json('card0001', require("../../assets/cards/json/0002.json") );
+  }
 
-    }
-
-
- public preload() {
-    const loaderPrefix: string = "../../assets/cards/json/0002.json"
-    //this.load.json('card0001', require('../../assets/cards/json/000'+ 2 +'.json') );
-   // this.load.json('card0001', require("../../assets/cards/json/0002.json") );
-    
- }
-
- public create() {
-//const card: ICard = this.cache.json.get("card0001");
-
-
-
- }
-
-
+  public create() {
+    const card: ICard = this.cache.json.get("card0001");
+    console.log(card);
+  }
 }
-
