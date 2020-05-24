8acd591ee4395ff2931a3a50d2738f97c5183cfe
unknown <rowanspenc@gmail.com>
2020-05-24 01:41:03 +0100

    Updated more classes

--- client-side/src/card/CardManager.ts
+++ client-side/src/card/CardManager.ts
@@ -9,10 +9,12 @@ export interface ICard {
 }
 
 // This class holds the information of all cards and basic functions for card manipulation.
-class CardManager {
+export default class CardManager {
     // Important information.
-    private readonly CARD_INFO_JSON_URL = "//www.empiraft.com/resources/card_game/json/?file=cards";
-    private readonly CARD_IMG_BASE_URL = "//www.empiraft.com/resources/card_game/img/?file=";
+    private readonly CARD_INFO_JSON_URL =
+        "//www.empiraft.com/resources/card_game/json/?file=cards";
+    private readonly CARD_IMG_BASE_URL =
+        "//www.empiraft.com/resources/card_game/img/?file=";
 
     // This array should contain all the card information there is in the whole game.
     private _arrCardList: Array;
@@ -25,28 +27,30 @@ class CardManager {
     // Obtains the image URL of a specific card from the array
     public getCardImgURL(index: number): string {
         if (index >= 0 && index < this.getCardListLength())
-            return this.CARD_IMG_BASE_URL + "000" + this._arrCardList[index].uid;
-        else
-            return "";
+            return (
+                this.CARD_IMG_BASE_URL + "000" + this._arrCardList[index].uid
+            );
+        else return "";
     }
 
     // Obtains the image URL of a specific card from the array
     public getCardName(index: number): string {
         if (index >= 0 && index < this.getCardListLength())
             return this._arrCardList[index].name;
-        else
-            return "";
+        else return "";
     }
 
     // Obtains the card information array from the internet.
     public async obtainCardList(): Promise<void> {
         await new Promise((resolve, reject) => {
-            readJSON(this.CARD_INFO_JSON_URL).then((result) => {
-                this._arrCardList = result.cards;
-                resolve();
-            }).catch((error) => {
-                reject(error);
-            });
+            readJSON(this.CARD_INFO_JSON_URL)
+                .then((result) => {
+                    this._arrCardList = result.cards;
+                    resolve();
+                })
+                .catch((error) => {
+                    reject(error);
+                });
         });
     }
 }
--- client-side/src/gameobjects/impl/MessageBox.ts
+++ client-side/src/gameobjects/impl/MessageBox.ts
@@ -1,22 +1,37 @@
-import {MainScene} from "../../scenes/MainScene";
+import { MainScene } from "../../scenes/MainScene";
 import Message from "../../game/Message";
-import {IGameObject} from "../IGameObject";
+import { IGameObject } from "../IGameObject";
 
-export default class MessageBox extends Phaser.GameObjects.Image implements IGameObject{
+export default class MessageBox extends Phaser.GameObjects.Image {
     public static readonly MAX_MESSAGE_LENGTH = 8;
     private _arrMessages: Message[] = [];
 
-    public constructor(scene: MainScene, x: number, y: number, backgroundTexture: string) {
+    public constructor(
+        scene: MainScene,
+        x: number,
+        y: number,
+        backgroundTexture: string
+    ) {
         super(scene, x, y, backgroundTexture);
         this.setOrigin(0, 1);
         this.scale = 4;
     }
 
-    public addMessage(message: string, color: string = 'black', bold: boolean = false): void {
-        let newMsgRect = new Message(this.scene, this.x + 50, this.y - 90, "[INFO] " + message, 18, bold);
+    public addMessage(
+        message: string,
+        color: string = "black",
+        bold: boolean = false
+    ): void {
+        let newMsgRect = new Message(
+            this.scene,
+            this.x + 50,
+            this.y - 90,
+            "[INFO] " + message,
+            18,
+            bold
+        );
         newMsgRect.setOrigin(0, 1);
-        if (color !== 'black')
-            newMsgRect.setColor(color);
+        if (color !== "black") newMsgRect.setColor(color);
 
         // Removes the top message.
         if (this._arrMessages.length == MessageBox.MAX_MESSAGE_LENGTH)
@@ -26,11 +41,13 @@ export default class MessageBox extends Phaser.GameObjects.Image implements IGam
         for (let i: number in this._arrMessages) {
             let msgRect = this._arrMessages[i];
             msgRect.y -= newMsgRect.height;
-            msgRect.alpha = 1 - (1 / MessageBox.MAX_MESSAGE_LENGTH) * (this._arrMessages.length - i);
+            msgRect.alpha =
+                1 -
+                (1 / MessageBox.MAX_MESSAGE_LENGTH) *
+                    (this._arrMessages.length - i);
         }
         this._arrMessages.push(newMsgRect);
     }
 
-    onEnable() {
-    }
+    onEnable() {}
 }
